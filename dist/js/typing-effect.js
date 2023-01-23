de8e92c18f604505b096968ff01ee6b5291a6ced
Ahsan <ahsanaz461@gmail.com>
2023-01-23 19:15:13 +0700

    added blink caret animation

--- dist/js/typing-effect.js
+++ dist/js/typing-effect.js
@@ -51,6 +51,6 @@ window.onload = function() {
     // INJECT CSS
     var css = document.createElement("style");
     css.type = "text/css";
-    css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #000}";
+    css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #000; animation: blink-caret .5s step-end infinite; } @keyframes blink-caret { from, to { border-color: transparent } 50% { border-color: black}}";
     document.body.appendChild(css);
 };
